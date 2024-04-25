8ff501a6445aa0b68b9f00495e8dbe94a94bd602
mustafa <mustafa.917@outlook.com>
2024-04-25 21:29:07 +0600

    ....

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,9 +1,8 @@
 return {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-    },
-    config = function ()
-        vim.keymap.set('n', ';t', '<cmd>TroubleToggle<cr>', {silent = true, noremap = true})
-    end
+    opts = {},
+    config = function()
+        vim.keymap.set('n', '<leader>e', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
+    end,
 }
