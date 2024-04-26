18baa63a653610982cc8003335070148720885bd
mustafa <mustafa.917@outlook.com>
2024-04-27 01:29:23 +0600

    init lua type

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ require('v9.keymaps')
 require('v9.lazy')
 require('v9.colorscheme')
 require('v9.runcode')
-require("v9.templ")
+require("v9.tailwind-html-lspconfig")
 
 --------------------------------
 -- Function to navigate to the Tmux pane on the right
