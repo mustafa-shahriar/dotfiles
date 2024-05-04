d2814c44688b7a526a7de0c41ade8b4cb6db4a38
mustafa <mustafa.917@outlook.com>
2024-05-04 21:37:52 +0600

    all the colorschemes are now available in themery

--- nvim/lua/plugins/themery.lua
+++ nvim/lua/plugins/themery.lua
@@ -1,4 +1,33 @@
-return {
+local color_schemes_table = {
+    {
+        name = 'tokyonight',
+        colorscheme = 'tokyonight',
+    },
+    {
+        name = 'tokyonight-night',
+        colorscheme = 'tokyonight-night',
+    },
+    {
+        name = 'tokyonight-storm',
+        colorscheme = 'tokyonight-storm',
+    },
+    {
+        name = 'tokyonight-day',
+        colorscheme = 'tokyonight-day',
+    },
+}
+local color_schemes = vim.fn.getcompletion('', 'color')
+
+-- Create a table of themes with each theme having 'name' and 'colorscheme' keys
+for _, scheme in ipairs(color_schemes) do
+    local theme = {
+        name = scheme,
+        colorscheme = scheme,
+    }
+    table.insert(color_schemes_table, theme)
+end
+
+local config = {
     'zaldih/themery.nvim',
     cmd = 'Themery',
     config = function()
@@ -8,72 +37,11 @@ return {
         end
 
         themery.setup({
-            themes = {
-                --{
-                --name = 'material',
-                --colorscheme = 'material',
-                --before = [[vim.g.material_style = 'darker']]
-                --},
-                -- {
-                --     name = 'vscode',
-                --     colorscheme = 'vscode',
-                -- },
-                -- {
-                --     name = 'catppuccin',
-                --     colorscheme = 'catppuccin',
-                -- },
-                -- {
-                --     name = 'catppuccin-mocha',
-                --     colorscheme = 'catppuccin-mocha',
-                -- },
-                -- {
-                --     name = 'catppuccin-frappe',
-                --     colorscheme = 'catppuccin-frappe',
-                -- },
-                -- {
-                --     name = 'catppuccin-latte',
-                --     colorscheme = 'catppuccin-latte',
-                -- },
-                -- {
-                --     name = 'catppuccin-macchiato',
-                --     colorscheme = 'catppuccin-macchiato',
-                -- },
-                {
-                    name = 'tokyonight',
-                    colorscheme = 'tokyonight',
-                },
-                {
-                    name = 'tokyonight-night',
-                    colorscheme = 'tokyonight-night',
-                },
-                {
-                    name = 'tokyonight-storm',
-                    colorscheme = 'tokyonight-storm',
-                },
-                {
-                    name = 'tokyonight-day',
-                    colorscheme = 'tokyonight-day',
-                },
-                -- {
-                --     name = 'GithubDark',
-                --     colorscheme = 'github_dark',
-                -- },
-                -- {
-                --     name = 'github_dark_dimmed',
-                --     colorscheme = 'github_dark_dimmed',
-                -- },
-                --
-                -- {
-                --     name = 'github_dark_default',
-                --     colorscheme = 'github_dark_default',
-                -- },
-                -- {
-                --     name = 'github_dark_colorblind',
-                --     colorscheme = 'github_dark_colorblind',
-                -- },
-            },
+            themes = color_schemes_table,
             themeConfigFile = '~/.config/nvim/lua/v9/colorscheme.lua',
             livePreview = true,
         })
     end,
 }
+
+return config
