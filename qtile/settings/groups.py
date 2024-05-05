c702e1c7820516828cf619bd76b5bd49ed2bd746
mustafa <mustafa.917@outlook.com>
2024-05-06 00:19:02 +0600

    wow...

--- qtile/settings/groups.py
+++ qtile/settings/groups.py
@@ -12,20 +12,12 @@ from settings.keys import mod, keys
 #    "   ", "  ", "   ", "   ", "   ", " ﲵ  ", "   ", "   ", "   ",
 #]]
 #
-#for i, group in enumerate(groups):
-#    actual_key = str(i + 1)
-#    keys.extend([
-#        # Switch to workspace N
-#        Key([mod], actual_key, lazy.group[group.name].toscreen()),
-#        # Send window to workspace N
-#        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
-#    ])
-
 #groups = [Group(i) for i in ["   ", "  ", "   ", "   ", "   ", " ﲵ  ", "   ", "   ", "   ",]
 #]
+# groups = [Group(i) for i in ["   ", "  ", "  "," 󰭤 ", "  "]
+# ]
 
-groups = [Group(i) for i in ["   ", "  ", "  "," 󰭤 ", "  "]
-]
+groups = [Group(i) for i in ["1", "2", "3","4", "5"]]
 group_hotkyes="asdfg"
 mod1 = "mod1"
 
--- qtile/settings/widgets.py
+++ qtile/settings/widgets.py
@@ -18,7 +18,7 @@ def separator():
     return widget.Sep(**base(), linewidth=0, padding=5)
 
 
-def icon(fg='text', bg='dark', fontsize=16, text="?"):
+def icon(fg='text', bg='dark', fontsize=17, text="?"):
     return widget.TextBox(
         **base(fg, bg),
         fontsize=fontsize,
@@ -42,7 +42,7 @@ def workspaces():
         widget.GroupBox(
             **base(fg='light'),
             font='sans',
-            fontsize=19,
+            fontsize=12,
             margin_y=3,
             margin_x=0,
             padding_y=8,
@@ -61,7 +61,9 @@ def workspaces():
             disable_drag=True
         ),
         separator(),
-        widget.WindowName(**base(fg='focus'), fontsize=14, padding=5),
+        separator(),
+        separator(),
+        widget.WindowName(**base(fg='focus'), fontsize=12, padding=5),
         separator(),
     ]
 
@@ -71,47 +73,16 @@ primary_widgets = [
 
     separator(),
 
-    #powerline('color4', 'dark'),
-
-    #widget.TextBox(**base(bg='color4'), text=' rchLinux'),
-
-    # icon(bg="color4", text=' '), # Icon: nf-fa-download 
-    # widget.CheckUpdates(
-    #     background=colors['color4'],
-    #     colour_have_updates=colors['text'],
-    #     colour_no_updates=colors['text'],
-    #     no_update_string='0',
-    #     display_format='{updates}',
-    #     update_interval=1800,
-    #     custom_command='checkupdates',
-    # ),
-
-    #powerline('color3', 'color4'),
-
-    # icon(bg="color1", text=''),  # Icon: nf-fa-feed
-    # widget.Net(**base(bg='color1'),format='{down:.0f}{down_suffix} ↓↑ {up:.0f}{up_suffix}'),
-
-    #powerline('color2', 'color3'),
-    
     icon(bg="color2", text='󰍛'),  # Icon: nf-fa-feed
     widget.Memory(**base(bg='color2'),format="{MemPercent}%"),
-    #powerline('color2', 'color3'),
 
+    separator(),
     icon(bg="color1", text='󰻠'),  # Icon: nf-fa-feed
     widget.CPU(**base(bg='color1'), format="{load_percent}%"),
 
-    #powerline('color2', 'color3'),
-
-
-    #widget.CurrentLayout(**base(bg='color2'), padding=5),
-
-    #powerline('color1', 'color2'),
-
-    #icon(bg="color1", fontsize=17, text=' '), # Icon: nf-mdi-calendar_clock
+    separator(),
     PulseVolumeExtra(
         **base(bg='color2'),
-        #bar_background="#0f101a",
-        #bar_colour="#334148",
         hide_after=99999999,
         hide_interval=99999999,
         bar_colour_loud="#949cbb",
@@ -119,14 +90,19 @@ primary_widgets = [
         bar_colour_normal="#949cbb",
     ),
 
+    separator(),
     widget.Clock(**base(bg='color1'), format='%d/%m/%y'),
+    separator(),
     widget.Clock(**base(bg='color2'), format='%I:%M:%S'),
 
     # widget.Systray(background=colors['dark'], padding=5),
     # widget.CurrentLayoutIcon(**base(bg='color2'), scale=0.65),
     # widget.KeyboardLayout(**base(bg="color1"),configured_keyboards=['en','ar']),
+    separator(),
     powerline('dark', 'color2'),
+    separator(),
     widget.QuickExit(**base(bg="color1") ,default_text="  ",fontsize="18"),
+    separator(),
 
 ]
 
@@ -150,7 +126,7 @@ secondary_widgets = [
 
 widget_defaults = {
     'font': 'sans',
-    'fontsize': 15,
+    'fontsize': 13,
     'padding': 3,
 }
 extension_defaults = widget_defaults.copy()
