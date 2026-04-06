d57561a06fc1c44d009da74390116070932774a7
mustafa-shahriar <mostafashahriar598@gmail.com>
2026-04-06 16:07:51 +0600

    nvim config updated to 0.12

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,14 +5,3 @@ require 'config.textyank'
 require 'config.indent'
 require 'config.keymaps'
 require 'config.tmux'
-
--- require('lspconfig').denols.setup {
---     on_attach = on_attach,
---     root_dir = require('lspconfig').util.root_pattern('deno.json', 'deno.lock'),
--- }
---
--- require('lspconfig').ts_ls.setup {
---     on_attach = on_attach,
---     root_dir = require('lspconfig').util.root_pattern 'package.json',
---     single_file_support = false,
--- }
